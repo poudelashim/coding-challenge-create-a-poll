aecd2ee01582271c73393c75718cefeec20cd89c
Ashim Poudel <poudelashim26@gmail.com>
2022-09-14 11:58:58 +1000

    getting answer

--- script.js
+++ script.js
@@ -34,4 +34,16 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    //get answer
+    const answers = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(What is your number?)`
+      )
+    );
+    console.log(answers);
+
+    //Register answer
+  },
 };
+// poll.registerNewAnswer();
